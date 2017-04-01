4e340eec88be29e83dbdacba2672ab269256b892
Deepanshu Sinha <107sinha@gmail.com>
2017-04-01 16:32:10 +0530

    Added TODO

--- app/Main.elm
+++ app/Main.elm
@@ -2,5 +2,14 @@ module Main exposing (..)
 
 import Html
 
+
+-- TODO: create type alias Model
+-- TODO: create model
+-- TODO: create type Msg with single constructor NoOp
+-- TODO: create update function that takes msg and model, and returns model
+-- TODO: create view
+
+
 main =
- Html.text "hello world"
+    -- update main to use Html.beginnerProgram
+    Html.text "hello world"
